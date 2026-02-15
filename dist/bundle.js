ee312e5a15c23bfeb460600231ca427838e1b386
Sebastian Münkel <mail@smuenkel.com>
2026-02-15 10:43:39 +0100

    debug: cheap Spider Lair for testing (1e6 parts, 1e4 brains, 30s)
    This commit was made using the assistance of the LLM system Claude Code.

--- dist/bundle.js
+++ dist/bundle.js
@@ -2009,9 +2009,9 @@ var Incremancer;
             parts: 1e18
           }, 240, 1, 1, 1, 303, "Using all these stored brains allows us to harness their raw computational power for even more innovations!  Storage tanks resting on bedrock is as far as we can go, doubling storage", "New upgrades are available in the shop!"),
           new Construction(305, "Spider Lair", this.constructionTypes.spiderLair, {
-            parts: 1e14,
-            brains: 1e9
-          }, 300, 1, 1, 1, 211, "Construct a dark lair beneath the graveyard where spiders can be bred and trained. These arachnid servants will serve as utility collectors, draining energy to sustain themselves.", "Spider Lair constructed! Spiders are now available in the graveyard menu.")
+            parts: 1e6,
+            brains: 1e4
+          }, 30, 1, 1, 1, 211, "Construct a dark lair beneath the graveyard where spiders can be bred and trained. These arachnid servants will serve as utility collectors, draining energy to sustain themselves.", "Spider Lair constructed! Spiders are now available in the graveyard menu.")
         ],
 
         this.upgrades = [new UpgradeDef(1, "Bloodthirst", this.types.damage, this.costs.blood, 50, 1.2, 1, 40, "Your zombies thirst for blood and do +1 damage for each rank of Bloodthirst.", null, null),
