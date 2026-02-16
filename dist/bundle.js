5339910b7631e8763cd552ca4d84e4b1582e3d39
Sebastian Münkel <mail@smuenkel.com>
2026-02-16 08:41:24 +0100

    feat: boss rush offer moved to level 5, forced 10s delay on level 5 complete
    - Boss rush offer button now appears after level 5 instead of level 50
    - Level 5 completion screen stays for 10 seconds regardless of skip-delay setting
    
    This commit was made using the assistance of the LLM system Claude Code.

--- dist/bundle.js
+++ dist/bundle.js
@@ -1385,6 +1385,13 @@ var Incremancer;
       if (this.persistentData.autoStartWait == !1 && this.currentState != this.states.levelCompleted && this.currentState != this.states.failed) {
         this.startTimer = 0
       }
+      if (this.level === 5 && this.currentState === this.states.levelCompleted && !this._level5TimerSet) {
+        this.startTimer = 10;
+        this._level5TimerSet = true;
+      }
+      if (this.level !== 5 || this.currentState !== this.states.levelCompleted) {
+        this._level5TimerSet = false;
+      }
       this.spells.updateSpells(e), e *= this.gameSpeed, this.hidden && U(e, this.app), this.partFactory.update(e), this.autoRemoveCollectorsHarpies(), this.addEnergy(this.getEnergyRate() * e), this.currentState == this.states.playingLevel && (this.addBones(this.bonesRate * e), this.addBrains(this.brainsRate * e), this.upgrades.updateRunicSyphon(this.runicSyphon), this.lastSave + 3e4 < t && (this.saveData(), this.lastSave = t), this.lastPlayFabSave + 12e5 < t && this.saveToPlayFab(), this.getHumanCount() <= 0 && (this.endLevelTimer < 0 ? (this.isBossStage(this.level) && this.trophies.doesLevelHaveTrophy(this.level) && this.trophies.trophyAquired(this.level), this.prestigePointsEarned = this.prestigePointsForLevel(this.level), this.currentState = this.states.levelCompleted, this.levelResourcesAdded = !1, this.calculateEndLevelBones(), this.calculateEndLevelZombieCages(), this.persistentData.levelsCompleted.includes(this.level) || (this.addPrestigePoints(this.prestigePointsForLevel(this.level)), this.persistentData.levelsCompleted.push(this.level)), this.persistentData.levelUnlocked = this.level + 1, (!this.persistentData.allTimeHighestLevel || this.level > this.persistentData.allTimeHighestLevel) && (this.persistentData.allTimeHighestLevel = this.level, window.kongregate && window.kongregate.stats.submit("level", this.persistentData.allTimeHighestLevel))) : this.endLevelTimer -= e), this.upgrades.updateConstruction(e), this.upgrades.updateAutoUpgrades(), this.creatureFactory.update(e), this.updateNetLauncher(e)), this.currentState == this.states.levelCompleted && (this.startTimer -= e);
       this.currentState == this.states.levelCompleted && this.startTimer < 0 && this.persistentData.autoStart && !this.bossRushMode && this.startLevel(this.level);
       this.currentState == this.states.levelCompleted && this.startTimer < 0 && this.bossRushMode && this.startLevel(Math.ceil((this.level + 1) / 50) * 50),
